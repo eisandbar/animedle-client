5fe3d360b9128d44aa601cf6c9985d6046775e09
eisandbar <eisandbar@gmail.com>
2024-02-09 10:39:45 -0500

    fix(client): Fix nan in average guesses
    Added a check if the number of games is 0, if so, then return 0

--- src/components/statsBox.tsx
+++ src/components/statsBox.tsx
@@ -21,7 +21,7 @@ export const StatsBox = ({ stats }: BoxProps) => {
                 <b>Games finished: {stats.games}</b>
             </p>
             <p>
-                <b>Average guesses: {(stats.guesses / stats.games).toFixed(1)} </b>
+                <b>Average guesses: {stats.games == 0 ? 0 : (stats.guesses / stats.games).toFixed(1)} </b>
             </p>
             <p>
                 <b>Streak: {stats.streak}</b>
